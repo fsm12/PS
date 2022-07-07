173f68591b92fe5fe5f702bd0e664788e478bdd0
박소미 <74345771+fsm12@users.noreply.github.com>
2022-07-07 11:46:03 +0900

    [Silver V] Title: 셀프 넘버, Time: 616 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
@@ -0,0 +1,12 @@
+def d():
+    m = []
+    for i in range(1,10000):
+        k = list(str(i))
+        for j in k:
+            i += int(j)
+        m.append(i)
+        
+    for p in range(1,10000):
+        if p not in m:
+            print(p)
+d()
