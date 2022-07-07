e552822092957a12cbcc63555f3ae1abd49b42ac
박소미 <74345771+fsm12@users.noreply.github.com>
2022-07-07 11:35:49 +0900

    [Bronze IV] Title: 주사위 세개, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -0,0 +1,2 @@
+a,b,c=map(int, input().split())
+print(10000+a*1000 if a==b==c else (1000+a*100 if a==b or a==c else (1000+b*100 if b==c else (a*100 if a>b and a>c else(b*100 if b>a and b>c else c*100)))))
