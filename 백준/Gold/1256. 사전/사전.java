5c246715dc01e319f4d47e1bf387f9ca408629bc
SoMi Park <74345771+fsm12@users.noreply.github.com>
2022-07-27 22:30:12 +0900

    [Gold II] Title: 사전, Time: 132 ms, Memory: 14372 KB -BaekjoonHub

--- 백준/Gold/1256. 사전/사전.java
+++ 백준/Gold/1256. 사전/사전.java
@@ -1,56 +1,64 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-public class Main {
-    static double[][] dp;
-    static BufferedWriter answer = new BufferedWriter(new OutputStreamWriter(System.out));
-    public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(bf.readLine());
-
-        int N,M,K;
-	    N = Integer.parseInt(st.nextToken());
-	    M = Integer.parseInt(st.nextToken());
-	    K = Integer.parseInt(st.nextToken());
-        dp = new double[N + 1][M + 1];
-
-
-        if(K>numWord(N,M)) {
+public class Main{
+	static int N,M,K;
+	static int[][] dp = new int[201][201];
+	static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws IOException {
+		//System.setIn(new FileInputStream("src/input.txt"));
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		
+		if (K > combination(N + M, M)) {
             System.out.println("-1");
-        }else {
-            getWord(N,M,K-1);
+        } else {
+        	query(N,M,K);
+        	System.out.println(sb.toString());
         }
-        answer.write("\n");
-    answer.close();
-    }
-    static double numWord(int A, int Z){
-        if(A==0||Z==0)
-            return 1;
-        if(dp[A][Z]!=0)
-            return dp[A][Z];
-        return dp[A][Z]= Double.min(numWord(A-1,Z)+numWord(A,Z-1), 1000000001);
-    }
-
-    static void getWord(int A, int Z, double before) throws IOException {
-        if(A==0) {
-            for (int i = 0; i < Z; i++)
-                answer.write("z");
-            return;
-        }
-        if(Z==0){
-            for (int i = 0; i < A; i++) 
-                answer.write("a");
-            return;
-            
-        }
-        double tmp = numWord(A-1, Z);
-
-        if(before < tmp){
-            answer.write("a");
-            getWord(A-1,Z,before);
-        }else {
-            answer.write("z");
-            getWord(A,Z-1,before-tmp);
-        }
-    }
+	}
+	
+	public static void query(int n, int m, int k) {
+		// 단어 끝에 도달
+		if(n+m == 0) {
+			return;
+		}
+		// z만 남은경우
+		else if(n==0){
+			sb.append('z');
+            query(n, m- 1, k);
+		}
+		// a만 남은경우
+		else if(m==0){
+			sb.append('a');
+	        query(n - 1, m, k);
+		}
+		// a, z 둘다 남은 경우 a를 고를 때 조합 수를 보고 판단
+		else {
+			int leftCnt = combination(n+m-1,m);
+			if (leftCnt >= k) {
+				sb.append('a');
+				query(n-1, m, k);
+			}else {
+				sb.append('z');
+				query(n, m-1, k-leftCnt);
+			}
+		}
+	}
+	
+	public static int combination(int n , int r) {
+		if(n==r || r==0) {
+			return 1;
+		}else if(dp[n][r] !=0){
+			return dp[n][r];
+		}else {
+			return dp[n][r] = Math.min((int)1e9, combination(n-1, r)+combination(n-1,r-1));
+		}
+	}
 }
