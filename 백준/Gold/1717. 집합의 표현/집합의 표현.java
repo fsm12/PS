b5e0f0ec41b244a05f82f012ebccf0334d26eea1
SoMi Park <74345771+fsm12@users.noreply.github.com>
2022-07-28 09:44:36 +0900

    [Gold IV] Title: 집합의 표현, Time: 1172 ms, Memory: 53984 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.java
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -1,46 +1,52 @@
-import java.io.*;
 import java.util.*;
-public class Main {
-    static int[] parent;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+import java.io.*;
 
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        parent = new int[N+1];
+public class Main{
+	static int N,M,w,a,b;
+	static int[] parent;
+	
+	public static void main(String[] args) throws IOException {
+		//System.setIn(new FileInputStream("src/input.txt"));
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		init(N);
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			w = Integer.parseInt(st.nextToken());
+			a = Integer.parseInt(st.nextToken());
+			b = Integer.parseInt(st.nextToken());
+			
+			if(w==1) {
+				// find 연산1
+				System.out.println((find(a)==find(b))?"YES":"NO");
+			}else {
+				// union 연산
+				union(a,b);
+			}
+		}
+	}
 
-        initialize(N);
-        for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
-            int cond = Integer.parseInt(st.nextToken());
-            if(cond==1){
-                if(find(Integer.parseInt(st.nextToken())) == find(Integer.parseInt(st.nextToken()))){
-                    System.out.println("YES");
-                }else{
-                    System.out.println("NO");
-                }
-            }else if(cond==0){
-                union(Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken()));
-            }
-        }
+	private static void init(int N) {
+		parent = new int[N+1];
+		for (int n = 0; n <= N; n++) {
+			parent[n]=n;
+		}
+	}
 
-    }
-    public static void initialize(int N){
-        for (int i = 0; i <= N; i++) {
-            parent[i] = i;
-        }
-    }
-    public static void union(int a, int b){
-        int aRoot = find(a);
-        int bRoot = find(b);
-        parent[aRoot] = bRoot;
-    }
-    public static int find(int a){
-        if(parent[a]==a){
-            return a;
-        }else{
-            return parent[a] = find(parent[a]);
-        }
-    }
+	private static void union(int a, int b) {
+		int rootA = find(a);
+		int rootB = find(b);
+		parent[rootA] = rootB;
+		
+	}
+
+	private static int find(int a) {
+		if(parent[a] == a) 
+			return a;
+		return parent[a] = find(parent[a]);
+	}
 }
