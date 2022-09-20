10f372d9741715602edbca2d21c9bb81d227b3a1
SoMi Park <74345771+fsm12@users.noreply.github.com>
2022-09-20 22:32:29 +0900

    [Gold III] Title: 다리 만들기, Time: 380 ms, Memory: 32564 KB -BaekjoonHub

--- 백준/Gold/2146. 다리 만들기/다리 만들기.py
+++ 백준/Gold/2146. 다리 만들기/다리 만들기.py
@@ -1,3 +1,6 @@
+import sys
+from collections import deque
+
 def labeling_island():
     num = 0
     for i in range(n):
@@ -12,11 +15,13 @@ def labeling_island():
                     r, c = queue.popleft()
                     check = False
                     for dr, dc in [[1, 0], [-1, 0], [0, 1], [0, -1]]:
-                        if 0<=r+dr<=n-1 and 0<=c+dc<=n-1:
-                            if board[r+dr][c+dc] == 1:
-                                board[r+dr][c+dc] = num
-                                queue.append([r+dr, c+dc])
-                            elif not board[r+dr][c+dc]:
+                        nr = r+dr
+                        nc = c+dc
+                        if 0<=nr<=n-1 and 0<=nc<=n-1:
+                            if board[nr][nc] == 1:
+                                board[nr][nc] = num
+                                queue.append([nr, nc])
+                            elif not board[nr][nc]:
                                 check = True
                     if check:
                         island[num].append([r, c])
@@ -29,26 +34,25 @@ def calc_distance(i, j, num):
     while queue:
         r, c = queue.popleft()
         for dr, dc in [[1, 0], [-1, 0], [0, 1], [0, -1]]:
-            if 0<=r+dr<=n-1 and 0<=c+dc<=n-1:
-                if 0 == board_[r+dr][c+dc] or board_[r][c] + 1 < board_[r+dr][c+dc]:
-                    queue.append([r+dr, c+dc])
-                    board_[r+dr][c+dc] = board_[r][c] + 1
+            nr = r + dr
+            nc = c + dc
+            if 0<=nr<=n-1 and 0<=nc<=n-1:
+                if 0 == board_[nr][nc] or board_[r][c] + 1 < board_[nr][nc]:
+                    queue.append([nr, nc])
+                    board_[nr][nc] = board_[r][c] + 1
 
-                elif board_[r+dr][c+dc] < 0 and board_[r+dr][c+dc] != num:
+                elif board_[nr][nc] < 0 and board_[nr][nc] != num:
                     board_[i][j] = num
                     dist = min(board_[r][c], dist)
-                    return 
+                    return
     board_[i][j] = num
 
-
-
-from collections import deque
 n = int(input())
 board = [list(map(int, input().split())) for _ in range(n)]
 island = {}
 labeling_island()
 
-dist = 10000
+dist = sys.maxsize
 for num, array in island.items():
     board_ = [x[:] for x in board]
     for i, j in array:
