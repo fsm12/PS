86158ad194f3cec4ce6f01f137c9853dc4aea427
SoMi Park <74345771+fsm12@users.noreply.github.com>
2022-10-03 22:37:34 +0900

    [Gold III] Title: 피리 부는 사나이, Time: 312 ms, Memory: 24980 KB -BaekjoonHub

--- 백준/Gold/16724. 피리 부는 사나이/피리 부는 사나이.java
+++ 백준/Gold/16724. 피리 부는 사나이/피리 부는 사나이.java
@@ -1,71 +1,49 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-public class Main{
-    static int N,M;
-    static char[][] map;
-    static int[] u;
+public class Main {
+    static int N,M,cnt=0,step=1;
+    static String[] map;
+    static int[][] vis;
+    static int[] dx = {-1, 1, 0, 0}, dy = {0, 0, -1, 1};
+    static int dir(char c) {
+        switch (c) {
+            case 'U': return 0;
+            case 'D': return 1;
+            case 'L': return 2;
+            case 'R': return 3;
+        }
+        return -1;
+    }
 
     public static void main(String[] args) throws IOException {
         //System.setIn(new FileInputStream("src/input.txt"));
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        map = new char[N][M];
-        u = new int[N*M];
-        for (int i = 0; i < N*M; i++) {
-            u[i] = i;
-        }
-        for (int i = 0; i < N; i++) {
-            map[i] = br.readLine().toCharArray();
-        }
+        map = new String[N];
+        for (int n = 0; n < N; n++)
+            map[n] = br.readLine();
 
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                int curIdx = i*M+j;
-                int nextIdx = findIdx(i,j);
-                if(findRoot(curIdx) != findRoot(nextIdx)){
-                    union(curIdx, nextIdx);
+        vis = new int[N][M];
+        for (int n = 0; n < N; n++) {
+            for (int m = 0; m < M; m++) {
+                if (vis[n][m] == 0) {
+                    int x = n;
+                    int y = m;
+                    while (vis[x][y] == 0) {
+                        vis[x][y] = step;
+                        x += dx[dir(map[x].charAt(y))];
+                        y += dy[dir(map[x].charAt(y))];
+                    }
+                    if (vis[x][y] == step)
+                        cnt++;
                 }
+                step++;
             }
         }
-        HashSet<Integer> set = new HashSet<>();
-        for (int i = 0; i < u.length; i++) {
-            set.add(findRoot(i));
-        }
-        System.out.println(set.size());
-    }
-
-    private static int findIdx(int i, int j) {
-        if(map[i][j] =='U'){
-            i--;
-        }else if(map[i][j] =='D'){
-            i++;
-        }else if(map[i][j] =='L'){
-            j--;
-        }else if(map[i][j] =='R'){
-            j++;
-        }
-        return i*M+j;
-    }
-
-    private static void union(int curNode, int nextNode) {
-        int cur = findRoot(curNode);
-        int next = findRoot(nextNode);
-        if(curNode>nextNode){
-            u[cur] = next;
-        }else{
-            u[next] = cur;
-        }
-    }
-
-    private static int findRoot(int idx) {
-        if(u[idx] == idx)
-            return idx;
-        else
-            return u[idx] = findRoot(u[idx]);
+        System.out.println(cnt);
     }
 }
