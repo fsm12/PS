5bed3be8b10dfa102207cae4e76002f27894619d
SoMi Park <74345771+fsm12@users.noreply.github.com>
2022-10-07 20:10:21 +0900

    [Gold V] Title: 감소하는 수, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Gold/1038. 감소하는 수/감소하는 수.py
+++ 백준/Gold/1038. 감소하는 수/감소하는 수.py
@@ -1,21 +1,21 @@
 from itertools import combinations
 
-N = int(input()) 
+N = int(input())
 ans = []
 
 dataset = [i for i in range(10)]
 for i in range(1, 11):
-    tmp = list(map(list, combinations(dataset, i)))
-        
-    for j in range(len(tmp)):
-        tmp[j] = sorted(tmp[j], reverse=True)
-    
-    for j in range(len(tmp)):
-        ans.append(int(''.join(map(str, tmp[j]))))
+    # 해당 자릿수 만큼 조합을 생성
+    comb_list = list(map(list, combinations(dataset, i)))
 
+    # 조합을 뽑으면 뒤로갈수록 커지는 식으로 생성되므로, 반대로 정렬하면 큰수가 앞에오도록 뽑을 수 있다 
+    for j in range(len(comb_list)):
+        comb_list[j] = sorted(comb_list[j], reverse=True)
+
+    # 뽑은 숫자 한자리 들을 모아서 한 숫자로 만든뒤 append 연산 ex) [1,2,3,4] -> 1234
+    for j in range(len(comb_list)):
+        ans.append(int(''.join(map(str, comb_list[j]))))
+
+# 정렬해야 커지는 순서대로 위치를 잡을 수 있으므로, 인덱스만으로 증가하는 수를 뽑을 수 있다.
 ans.sort()
-
-if N >= len(ans):
-    print(-1)
-else:
-    print(ans[N])
+print(-1 if N >= len(ans) else ans[N])
