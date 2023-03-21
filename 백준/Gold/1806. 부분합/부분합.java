44896ae0df7d146dcda91ccb22209be669abf523
SoMi Park <74345771+fsm12@users.noreply.github.com>
2023-03-21 13:55:52 +0900

    [Gold IV] Title: 부분합, Time: 284 ms, Memory: 23708 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -1,32 +1,55 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-public class Main {
-	public static void main(String[] args) throws IOException{
+/* 0.5 초(Java 1초)	128 MB
+ * 
+ * [입력]
+ * 1> N (10 ≤ N < 100,000)과 S (0 < S ≤ 100,000,000)
+ * 1> N개로 구성된 수열
+ * => 구하고자 하는 최소의 길이(불가능 0)
+ * 
+ * sol) 투포인터
+ * 
+ */
+
+public class Main{
+	static int N,S,ans;
+	static int[] arr;
+	
+	public static void main(String[] args) throws Exception {
+		// System.setIn(new FileInputStream("src/input.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		S = Integer.parseInt(st.nextToken());
 		
-		int n = Integer.parseInt(st.nextToken());
-		int s = Integer.parseInt(st.nextToken());
-		
-		int[] arr = new int[n+1];
+		arr = new int[N];
 		st = new StringTokenizer(br.readLine());
-		for(int i=0; i<n; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
+		for(int n=0; n<N; n++) {
+			arr[n] = Integer.parseInt(st.nextToken());
 		}
 		
-		int start = 0;
-		int end = 0;
-		int len = Integer.MAX_VALUE;
-		int sum = 0;
-		while(start <= end && end <= n) {
-			if(sum < s) {
-				sum += arr[end++];
-			} else if(sum >= s) {
-				len = Math.min(len, end-start);
-				sum -= arr[start++];
-			}  
+		ans = N+1;
+		int sp=0, ep=0, cur=arr[sp];
+		while(sp<=ep) {
+			if(cur>=S) {
+				ans = Math.min(ans, ep-sp+1);
+				if(ans==1)
+					break;
+				cur -= arr[sp];
+				sp+=1;
+			}else {
+				ep+=1;
+				if(ep>=N) {
+					if(ans == N+1) {
+						ans = 0;
+					}
+					break;
+				}
+				cur += arr[ep];
+			}
 		}
-		System.out.println(len==Integer.MAX_VALUE ? 0 : len);
+		System.out.println(ans);
 	}
 }
