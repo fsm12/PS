2fbe814133dac09de2780d22766064d93d39cbb0
SoMi Park <74345771+fsm12@users.noreply.github.com>
2023-04-10 19:00:31 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 124 ms, Memory: 14080 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+/*
+ * 1 초	512 MB
+ * 
+ * 1> n (1 ≤ n ≤ 500,000)
+ * => 수행 횟수, 다항식으로 나타내었을 때, 최고차항의 차수
+ * 
+ * sol)
+ * 어떤 N이 들어오더라도 한번만 수행됨
+ * 고정된 출력값
+ */
+
+public class Main{
+	
+	public static void main(String[] args) {
+		System.out.println("1\n0");
+	}
+}
