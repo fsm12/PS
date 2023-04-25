91c3b8f281c3b3f053c1d31d0b24ccde603a8348
SoMi Park <74345771+fsm12@users.noreply.github.com>
2023-04-25 19:47:30 +0900

    [Silver IV] Title: 다음 소수, Time: 1404 ms, Memory: 33376 KB -BaekjoonHub

---
+++ 백준/Silver/4134. 다음 소수/다음 소수.py
@@ -0,0 +1,24 @@
+import sys
+import math
+
+# sys.stdin = open("input.txt")
+input = sys.stdin.readline
+
+def is_prime(x):
+    if x == 0 or x == 1:
+        return False
+    for i in range(2, int(math.sqrt(x))+1):
+        if x % i == 0:
+            return False
+    return True
+
+if __name__ == "__main__":
+    T = int(input())
+    for _ in range(T):
+        N = int(input())
+        while True:
+            if is_prime(N):
+                print(N)
+                break
+            else:
+                N += 1
