98e850e45fa3a66aaf396b12dd8f2f3e226227cd
SoMi Park <74345771+fsm12@users.noreply.github.com>
2023-06-04 19:07:04 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 1.89 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -1,49 +1,60 @@
-import java.util.*;
 
 /*
-오늘 날짜를 의미하는 문자열 today
-약관의 유효기간을 담은 1차원 문자열 배열 terms (약관종류[A~Z] 유효기간[1,100])
-수집된 개인정보의 정보를 담은 1차원 문자열 배열 privacies (날짜 약관종류)
-=> 파기해야 할 개인정보의 번호를 오름차순으로 1차원 정수 배열에 담아 return
+inp_today : 오늘날짜 문자열
+            "2022.05.19"
+inp_terms : 약관의 유효기간을 담은 1차원 문자열 배열
+            ["A 6", "B 12", ...] // 약관종류 유효기간
+            길이는 최대 20
+inp_privacies : 수집된 개인 정보를 담은 1차원 문자열 배열
+                ["2021.05.02 A", "2021.07.01 B", ...] // 날짜 약관종류
+                길이는 최대 100
 
-sol)
-해쉬맵으로 약관종류 : 유효기간 만들기
-privacies를 순회하며 유효기간을 넘겼다면 ans 배열에 담기
+2021년 1월 5일 => 12달 후 => 2022년 1월 4일까지 보관 가능
+모든달은 28일까지
 
+=> 파기해야 할 개인정보의 번호를 오름파순으로 1차원 배열에 담아 return
+
+풀이)
+오늘로부터 inp_terms에 속한 기한을 다 계산해서 A의 마감일은 언제이다로 매핑
+위에서 매핑한 값을 inp_privacies와 비교해서 term < privacies 이면 해당 번호 추가 => 어떤 약관인지를 바로 가져와야 하므로 탐색에 유용한 해시맵 사용
+
+ㄴ 날짜를 연산할 때 y, m, d 따로 가져오는 것보다 d로 통합해서 가져오는 것이 이득
 */
 
+
+import java.util.*;
+
 class Solution {
-    static Map <String, String> map;
-    static List<Integer> ans_list;
-    
-    public int[] solution(String today, String[] terms, String[] privacies) {
-        map = new HashMap<>();
-        for (int i = 0; i < terms.length; i++) {
-            map.put(terms[i].split(" ")[0], terms[i].split(" ")[1]);
+    public int[] solution(String inp_today, String[] inp_terms, String[] inp_privacies) {
+        Map <String, String> map = new HashMap<>();
+        
+        for (String terms : inp_terms) {
+            map.put(terms.split(" ")[0], terms.split(" ")[1]);
         }
 
-        ans_list = new ArrayList<>();
-        int year = Integer.parseInt(today.split("\\.")[0]);
-        int month = Integer.parseInt(today.split("\\.")[1]);
-        int day = Integer.parseInt(today.split("\\.")[2]);
+        int ty = Integer.parseInt(inp_today.split("\\.")[0]);
+        int tm = Integer.parseInt(inp_today.split("\\.")[1]);
+        int td = Integer.parseInt(inp_today.split("\\.")[2]);
+        
+        List<Integer> ans_list = new ArrayList<>();
+        for (int p=0; p < inp_privacies.length; p++) {
+            String pastDate = inp_privacies[p].split(" ")[0];
+            
+            int valid_gap = Integer.parseInt(map.get(inp_privacies[p].split(" ")[1]))*28;
+            int tp_gap = (ty - Integer.parseInt(pastDate.split("\\.")[0]))*28*12
+                + (tm - Integer.parseInt(pastDate.split("\\.")[1]))*28
+                + (td - Integer.parseInt(pastDate.split("\\.")[2]));
 
-        for (int i = 0; i < privacies.length; i++) {
-            String date = privacies[i].split(" ")[0];
-            int type = Integer.parseInt(map.get(privacies[i].split(" ")[1]))*28;
-
-            int num = (year - Integer.parseInt(date.split("\\.")[0]))*28*12
-                    + (month - Integer.parseInt(date.split("\\.")[1]))*28
-                    + (day - Integer.parseInt(date.split("\\.")[2]));
-
-            if (num >= type) {
-                ans_list.add(i+1);
+            if (tp_gap >= valid_gap) {
+                ans_list.add(p+1);
             }
         }
         
-        int[] ans = new int[ans_list.size()];
-        for(int i=0; i<ans_list.size(); i++){
-            ans[i] = ans_list.get(i);
+        int[] answer = new int[ans_list.size()];
+        int idx = 0;
+        for(int ans : ans_list){
+            answer[idx++] = ans;
         }
-        return ans;
+        return answer;
     }
 }
