8b6fe68ba0a9ef51bda34646f81bb9f848204ba4
SoMi Park <74345771+fsm12@users.noreply.github.com>
2023-06-06 23:06:02 +0900

    [level 1] Title: [1차] 비밀지도, Time: 1.67 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,20 +1,3 @@
-/*
-한곳이라도 벽이면 벽, 둘다 공백이면 공백으로 해독 => OR 연산
-
-*/
-
-// class Solution {
-//     public String[] solution(int N, int[] arr1, int[] arr2) {
-//         String[] answer = new String[N];
-//         for(int n=0; n<N; n++){
-//             answer[n] = Integer.toBinaryString(arr1[n] | arr2[n]).replace("1","#").replace("0"," ");
-//             System.out.println(arr1[n] | arr2[n]);
-//         }
-
-//         return answer;
-//     }
-// }
-
 class Solution {
     public String[] solution(int N, int[] arr1, int[] arr2) {
         String[] answer = new String[N];
@@ -23,7 +6,7 @@ class Solution {
             
             StringBuilder sb = new StringBuilder();
             for(int i=0; i<N-binary_str.length(); i++){
-                sb.insert(0," ");
+                sb.append(" ");
             }
             sb.append(binary_str);
             
