b5d8c62571ac2b73bb17f4281990bf4aa13f6559
SoMi Park <74345771+fsm12@users.noreply.github.com>
2023-06-07 23:10:03 +0900

    [level 2] Title: 스킬트리, Time: 0.14 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/lv2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/lv2/49993. 스킬트리/스킬트리.java
@@ -1,30 +1,6 @@
-/*
-
-*/
-import java.util.*;
-
 class Solution {
     public int solution(String skill, String[] skill_trees) {
-        int answer = skill_trees.length;
-        Map<Character, Integer> map = new HashMap<>();
-        for(int i=0; i<skill.length(); i++){
-            map.put(skill.charAt(i), i+1);
-        }
-        
-        for(String cs : skill_trees){
-            int order = 0;
-            for(int i=0; i<cs.length(); i++){
-                char cur = cs.charAt(i);
-                if(map.containsKey(cur)){
-                    if(order + 1 == map.get(cur)){
-                        order+=1;
-                    }else{
-                        answer-=1;
-                        break;
-                    }
-                }
-            }
-        }
+        int answer = -1;
         return answer;
     }
 }
