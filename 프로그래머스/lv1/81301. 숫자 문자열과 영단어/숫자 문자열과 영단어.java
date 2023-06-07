e4a48c1bac7a1203952fc685b6d4cd1cada29acc
SoMi Park <74345771+fsm12@users.noreply.github.com>
2023-06-07 19:00:24 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 2.18 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,9 +1,34 @@
+// class Solution {
+//     public int solution(String s) {
+//         String[] nums = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+//         for(int i=0; i<10; i++){
+//             s = s.replace(nums[i], i+"");
+//         }
+//         return Integer.parseInt(s);
+//     }
+// }
+
+import java.util.*;
 class Solution {
     public int solution(String s) {
-        String[] nums = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-        for(int i=0; i<10; i++){
-            s = s.replace(nums[i], i+"");
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("zero",0);
+        map.put("one",1);
+        map.put("two",2);
+        map.put("three",3);
+        map.put("four",4);
+        map.put("five",5);
+        map.put("six",6);
+        map.put("seven",7);
+        map.put("eight",8);
+        map.put("nine",9);
+        
+        for (String key : map.keySet()){
+            System.out.println("key: "+key);
+            s = s.replace(key,String.valueOf(map.get(key)));
         }
-        return Integer.parseInt(s);
+        
+        int answer = Integer.parseInt(s);
+        return answer;
     }
 }
