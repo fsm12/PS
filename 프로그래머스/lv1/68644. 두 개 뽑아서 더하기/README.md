fc2bd47b9c0c3b6d5ae9026cf639c2de8dc27b04
SoMi Park <74345771+fsm12@users.noreply.github.com>
2023-06-10 22:14:37 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 2.88 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/README.md
+++ 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 두 개 뽑아서 더하기 - 68644 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68644?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68644) 
 
 ### 성능 요약
 
-메모리: 76.7 MB, 시간: 0.53 ms
+메모리: 75 MB, 시간: 2.88 ms
 
 ### 구분
 
--- 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
+++ 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -1,31 +1,15 @@
 import java.util.*;
-
 class Solution {
     public int[] solution(int[] numbers) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        for(int node : numbers){
-             pq.add(node);
-         }
+        int[] answer = {};
+        Set<Integer> set = new HashSet<>();
         
-        int cnt = 0;
-        int[] possi_nums = new int[pq.poll()+pq.poll()+1];
-        for(int i=0; i<numbers.length; i++){
-            for(int j=i+1; j<numbers.length; j++){
-                if(possi_nums[numbers[i]+numbers[j]] == 1)
-                    continue;
-                possi_nums[numbers[i]+numbers[j]] = 1;
-                cnt += 1;
+        for(int i=0; i<numbers.length-1; i++) {
+            for(int j=i+1; j<numbers.length; j++) {
+                set.add(numbers[i] + numbers[j]);
             }
         }
-        
-        int[] answer = new int[cnt];
-        int idx = 0;
-        for(int i=0; i<possi_nums.length; i++){
-            if(possi_nums[i] == 1){
-                answer[idx] = i;
-                idx+=1;
-            }
-        }
-        return answer;
+        // ㅇㅇ
+        return set.stream().sorted().mapToInt(Integer::intValue).toArray();
     }
 }
