4b6ae271485c1fd44cc4fc98a995f7f250ef6572
SoMi Park <74345771+fsm12@users.noreply.github.com>
2023-06-17 19:51:45 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.80 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
+++ 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -1,15 +1,32 @@
 import java.util.*;
+
 class Solution {
     public int[] solution(int[] numbers) {
-        int[] answer = {};
-        Set<Integer> set = new HashSet<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for(int node : numbers){
+             pq.add(node);
+         }
         
-        for(int i=0; i<numbers.length-1; i++) {
-            for(int j=i+1; j<numbers.length; j++) {
-                set.add(numbers[i] + numbers[j]);
+        int cnt = 0, num_size = numbers.length, possi_size = pq.poll()+pq.poll()+1;
+        boolean[] possi_nums = new boolean[possi_size];
+        
+        for(int i=0; i<num_size; i++){
+            for(int j=i+1; j<num_size; j++){
+                if(possi_nums[numbers[i]+numbers[j]])
+                    continue;
+                possi_nums[numbers[i]+numbers[j]] = true;
+                cnt += 1;
             }
         }
-        // ㅇㅇ
-        return set.stream().sorted().mapToInt(Integer::intValue).toArray();
+        
+        int[] answer = new int[cnt];
+        int idx = 0;
+        for(int i=0; i<possi_size; i++){
+            if(possi_nums[i]){
+                answer[idx] = i;
+                idx+=1;
+            }
+        }
+        return answer;
     }
 }
