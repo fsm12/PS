c783393ef584fefe0284a72924583ccb6cf8bd9b
SoMi Park <74345771+fsm12@users.noreply.github.com>
2023-06-18 20:49:30 +0900

    [unrated] Title: 둘만의 암호, Time: 0.41 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.java
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.java
@@ -1,21 +1,17 @@
 import java.util.*;
 
 class Solution {
-    
     public String solution(String inp_s, String inp_skip, int inp_index) {
-        StringBuilder answer = new StringBuilder();
-        for(char s : inp_s.toCharArray()){
-             for (int index = 0; index < inp_index; index++) {
-                s += 1;
-                if (s > 'z') {
-                    s -= 26;
-                }
-                if (inp_skip.contains(String.valueOf(s))) {
-                    index--;
-                }
-            }
-            answer.append(s);
+        String alpha = "abcdefghijklmnopqrstuvwxyz";
+        for(String skip : inp_skip.split("")){
+            alpha = alpha.replace(skip, "");
         }
-        return answer.toString();
+        
+        int len = alpha.length();
+        StringBuilder ans = new StringBuilder();
+        for(String s : inp_s.split("")){
+            ans.append(alpha.charAt((alpha.indexOf(s) + inp_index)%len));
+        }
+        return ans.toString();
     }
 }
