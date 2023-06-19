492ab06c551561fc91f66ddbf49c6e7ba3dd2c21
SoMi Park <74345771+fsm12@users.noreply.github.com>
2023-06-19 20:18:30 +0900

    [unrated] Title: 콜라 문제, Time: 0.03 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.java
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.java
@@ -1,5 +1,14 @@
 class Solution {
     public int solution(int a, int b, int n) {
-        return (n - b) / (a - b) * b; 
+        int ans = 0;
+        int empty = n;
+        
+        while(empty >= a) {
+            int get = empty / a * b ;
+            
+            empty = empty % a + get;
+            ans += get;
+        }
+        return ans;
     }
 }
