ff197a817cab0d952fac16b785b85f5c94e80f7f
SoMi Park <74345771+fsm12@users.noreply.github.com>
2023-06-19 16:36:44 +0900

    [unrated] Title: 콜라 문제, Time: 0.03 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.java
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.java
@@ -1,12 +1,5 @@
-// 빈 a병을 가져다주면 새 b병을 얻음
 class Solution {
     public int solution(int a, int b, int n) {
-        int empty = n, ans = 0;
-        while(empty >= a){
-            empty-=a;
-            empty+=b;
-            ans += b;
-        }
-        return ans;
+        return (n - b) / (a - b) * b; 
     }
 }
