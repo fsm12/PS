b509fec2db0d245a745953f40ecde1af485f68eb
SoMi Park <74345771+fsm12@users.noreply.github.com>
2023-06-25 17:30:20 +0900

    [level 1] Title: 소수 찾기, Time: 9.65 ms, Memory: 53.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/README.md
+++ 프로그래머스/lv1/12921. 소수 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 소수 찾기 - 12921 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12921?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12921) 
 
 ### 성능 요약
 
-메모리: 52.9 MB, 시간: 13.75 ms
+메모리: 53.3 MB, 시간: 9.65 ms
 
 ### 구분
 
--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.java
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.java
@@ -1,17 +1,15 @@
 class Solution {
     public int solution(int n) {
         boolean[] isNotPrime = new boolean[n+1];
+        int ans = n-1;
         for(int i=2; i<=(int)Math.sqrt(n); i++){
             if(!isNotPrime[i]){
                 for(int j=i*i; j<=n; j+=i){
+                    if(!isNotPrime[j])
+                        ans -= 1;
                     isNotPrime[j] = true;
                 }
             }
-        }
-        
-        int ans = 0;
-        for(int i=2; i<=n; i++){
-            ans += isNotPrime[i]?0:1;
         }
         return ans;
     }
