534fe9309735d9392c05afaafd35b6d2d407085a
SoMi Park <74345771+fsm12@users.noreply.github.com>
2023-06-27 16:53:52 +0900

    [level 1] Title: 실패율, Time: 14.77 ms, Memory: 77.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42889. 실패율/실패율.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int N, int[] stages) {
+        Map<Integer, Double> map = new HashMap<>();
+        int max_depart = 0;
+        int[] cnt = new int[N+2];
+        for(int stage : stages){
+            cnt[stage]++;
+            max_depart = Math.max(max_depart, stage);
+        }
+        
+        int total = stages.length;
+        for(int n=1; n<N+1; n++){
+            if(cnt[n]==total && max_depart < n){
+                map.put(n, 0.0);
+            }else{
+                map.put(n, (double)cnt[n]/total);
+            }
+            System.out.println(cnt[n]+" "+total);
+            total -= cnt[n];
+        }
+        
+        List<Integer> list = new ArrayList<>(map.keySet());
+        Collections.sort(list, (o1,o2) -> Double.compare(map.get(o2), map.get(o1)));
+        return list.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
