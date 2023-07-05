6adaa09b2b656e2156f54eae22a37d7bb6eb3d81
SoMi Park <74345771+fsm12@users.noreply.github.com>
2023-07-05 17:51:22 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 95.54 ms, Memory: 96.9 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -3,22 +3,19 @@ import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
         Map<String, Integer> map = new HashMap<>();
-        for(String parti : participant){
-            map.put(parti,map.getOrDefault(parti,0)+1);
+        for (String parti : participant) {
+            map.put(parti, map.getOrDefault(parti, 0) + 1);
         }
-        
-        for(String com : completion){
+
+        for (String com : completion) {
             Integer cnt = map.get(com);
-            map.put(com, cnt-1);
-        }
-        
-        String ans = "";
-        for(String key : map.keySet() ){
-            if(0 < map.get(key)){
-                ans = key;
-                break;
+            if (cnt != null && cnt == 1) {
+                map.remove(com);
+            } else {
+                map.put(com, cnt - 1);
             }
         }
-        return ans;
+
+        return map.keySet().iterator().next();
     }
 }
