6d6653cf6b4ccdd5929dc7f1b7303027259e9054
SoMi Park <74345771+fsm12@users.noreply.github.com>
2023-07-06 22:33:10 +0900

    [unrated] Title: 문자열 나누기, Time: 4443.62 ms, Memory: 389 MB -BaekjoonHub

--- 프로그래머스/unrated/140108. 문자열 나누기/문자열 나누기.java
+++ 프로그래머스/unrated/140108. 문자열 나누기/문자열 나누기.java
@@ -1,29 +1,26 @@
 class Solution {
     public int solution(String s) {
-        int split_cnt = 1;
-        
-        char standard = s.charAt(0);
-        int std_cnt = 1, other_cnt = 0;
-        for(int i=1; i<s.length(); i++){
-            char c = s.charAt(i);
-            if(standard == c){
-                std_cnt++;
-            }else{
-                other_cnt++;
-            }
-            
-            if(std_cnt == other_cnt){
-                i++;
-                if(s.length() <= i)
-                    break;
-                standard = s.charAt(i);
-                split_cnt++;
-                
-                std_cnt = 1;
-                other_cnt = 0;
-            }
+        String[] arr = {};
+        int idx1 = 0;
+        int idx2 = 0;
+        int answer = 0;
+
+        for (int i=0; i<s.length(); i++){
+            arr = s.split("");
         }
-        
-        return split_cnt;
+
+        String first = arr[0];
+        for (int i=0; i<arr.length; i++){
+            if (idx1 == idx2){
+                answer++;
+                first = arr[i];
+            }
+            if (arr[i].equals(first))
+                idx1++;
+            else 
+                idx2++;
+        }
+
+        return answer;
     }
 }
