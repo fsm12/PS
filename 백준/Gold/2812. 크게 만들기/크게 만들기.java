17d0a444264e4c22074bbb1b1946b72e06e10cd8
SoMi Park <74345771+fsm12@users.noreply.github.com>
2023-09-04 18:03:14 +0900

    [Gold III] Title: 크게 만들기, Time: 316 ms, Memory: 20644 KB -BaekjoonHub

---
+++ 백준/Gold/2812. 크게 만들기/크게 만들기.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+	static int N,K,del=0,p=0;
+	static char[] stack;
+	static String num;
+	
+	public static void main(String[] args) throws Exception {
+		// System.setIn(new FileInputStream("input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+ 
+		num = br.readLine();
+		stack = new char[N+1];
+		for (int i = 0; i < N; i++) {
+            while (del < K && p!=0 && Integer.valueOf(stack[p-1]) < Integer.valueOf(num.charAt(i))) {
+                p-=1;
+                del++;
+            }
+            stack[p++] = num.charAt(i);
+        }
+		
+		StringBuilder sb = new StringBuilder();
+        for(int i=0; i<N-K; i++){
+            sb.append(stack[i]);
+        }
+		System.out.println(sb.toString());
+	}
+}
