db99db6b45c64f98d3f22895a91ce3b4e0a2ae22
fsm12 <74345771+fsm12@users.noreply.github.com>
2023-10-01 23:32:51 +0900

    [Bronze V] Title: 이상한 기호, Time: 124 ms, Memory: 14176 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15964. 이상한 기호/이상한 기호.java
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -2,16 +2,16 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int a,b;
+	static long a,b;
 
 	public static void main(String[] args) throws IOException {
 		// System.setIn(new FileInputStream("input.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		a = Integer.parseInt(st.nextToken());
-		b = Integer.parseInt(st.nextToken());
+		a = Long.parseLong(st.nextToken());
+		b = Long.parseLong(st.nextToken());
 		
-		System.out.println((int)(Math.pow(a, 2) - Math.pow(b, 2)));
+		System.out.println((long)(Math.pow(a, 2) - Math.pow(b, 2)));
 	}
 }
