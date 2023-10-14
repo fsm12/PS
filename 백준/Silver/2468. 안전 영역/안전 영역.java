814a10bf344383a34775b7a5e287fc5269170bc7
fsm12 <74345771+fsm12@users.noreply.github.com>
2023-10-14 22:51:31 +0900

    [Silver I] Title: 안전 영역, Time: 256 ms, Memory: 18260 KB -BaekjoonHub

---
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+	static int N,min=100,max=0,ans=1;
+	static int[][] map;
+	static boolean[][] vis;
+	static int[] dx = {-1,1,0,0}, dy = {0,0,-1,1};
+	
+	public static void main(String[] args) throws Exception{
+		// System.setIn(new FileInputStream("input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		N = Integer.parseInt(br.readLine());
+		
+		map = new int[N][N];
+		for(int i=0; i<N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for(int j=0; j<N; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+				max = Math.max(max, map[i][j]);
+				min = Math.min(min, map[i][j]);
+			}
+		}
+		
+		
+		for(int h=min; h<max; h++) {
+			int curAns = 0;
+			vis = new boolean[N][N];
+			for(int i=0; i<N; i++) {
+				for(int j=0; j<N; j++) {
+					if(!vis[i][j] && h<map[i][j])
+						curAns+=dfs(i,j,h);
+				}
+			}
+			ans = Math.max(ans, curAns);
+		}
+		System.out.println(ans);
+    }
+	
+	public static int dfs(int x, int y, int height) {
+		vis[x][y] = true;
+		for(int i=0; i<4; i++) {
+			int nx = x +dx[i];
+			int ny = y +dy[i];
+			
+			if(nx<0 || ny<0 || nx>N-1 || ny >N-1) 
+				continue;
+			if(vis[nx][ny]) continue;
+			if(map[nx][ny]> height) {
+				dfs(nx,ny, height);
+			}
+		}
+		return 1;
+	}
+}
