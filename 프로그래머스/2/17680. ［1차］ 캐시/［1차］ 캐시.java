f5073709c15cd4ff6d18fe72cbf91373476dd4b7
fsm12 <74345771+fsm12@users.noreply.github.com>
2023-12-04 23:49:23 +0900

    [level 2] Title: [1차] 캐시, Time: 1.75 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -1,24 +1,54 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
-class Solution {
-    public int solution(int cacheSize, String[] cities) {
-        if(cacheSize == 0)
-            return cities.length*5;
-        
-        LinkedList<String> cache = new LinkedList<>();
-        int ans = 0;
-        for(String s : cities){
-            s = s.toUpperCase();
-            if(cache.remove(s)){
-                ans+=1;
-            }else{
-                ans+=5;
-                if(cacheSize <= cache.size()){
-                    cache.remove(0);
-                }
-            }
-            cache.add(s);
+public class Solution {
+
+    static final int HIT = 1;
+    static final int MISS = 5;
+
+    public static int solution(int cacheSize, String[] cities) {
+        int answer = 0;
+        List<String> cache = new ArrayList<>();
+
+        if (cacheSize == 0) {
+            // 캐시 사이즈가 없는 경우 예외
+            return cities.length * MISS;
         }
-        return ans;
+        
+
+        for (String city : cities) {
+            // System.out.println(cache);
+            // 대소문자 통일
+            city = city.toLowerCase();
+
+            if (cache.size() < cacheSize) {
+                // 캐시 사이즈보다 작은 경우
+
+                if (cache.contains(city)) {
+                    // 캐시 사이즈보다 작지만, 캐시에 속한 경우
+                    answer += HIT;
+                } else {
+                    // 캐시 사이즈보다 작지만, 캐시에 없는 경우
+                    answer += MISS;
+                }
+                    cache.add(city);
+                continue;
+            }
+
+            // 캐시 사이즈보다 큰 경우
+            if (cache.contains(city)) {
+                // 캐시 사이즈보다 크면서 캐시 안에 존재하는 경우
+                cache.remove(city);
+                cache.add(city);
+                answer += HIT;
+            } else {
+                // 캐시 사이즈보다 크지만, 캐시 안에 도시가 존재하지 않는 경우
+                cache.remove(0);
+                cache.add(city);
+                answer += MISS;
+            }
+        }
+
+        return answer;
     }
 }
