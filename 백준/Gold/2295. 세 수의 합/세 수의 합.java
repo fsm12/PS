bea05c5747c3cadc9312e2303e057f741446ce0d
fsm12 <74345771+fsm12@users.noreply.github.com>
2024-01-31 10:22:02 +0900

    [Gold IV] Title: 세 수의 합, Time: 260 ms, Memory: 22316 KB -BaekjoonHub

--- 백준/Gold/2295. 세 수의 합/세 수의 합.java
+++ 백준/Gold/2295. 세 수의 합/세 수의 합.java
@@ -1,33 +1,44 @@
 import java.io.*;
 import java.util.*;
 
+/*
+ * 두 수를 고름 => N^2 = 1,000,000
+ * 
+ * 두 수를 더한 배열, 나머지 수 배열 => 합했을 때 수를 찾을 수 있는가
+ * A+B+C = want
+ * A+B = want-C ==> want-C가 A+B 집합에 있는가?
+ */
+
+
 public class Main {
+	static int N;
+	static int[] origin, twoSum;
     
     public static void main(String[] args) throws IOException {
-    	// System.setIn(new FileInputStream("src/input.txt"));
+    	// System.setIn(new FileInputStream("src/origin.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int[] input = new int[N];
-        int[] sumArr = new int[N*N];
+        N = Integer.parseInt(br.readLine());
+        origin = new int[N];
+        twoSum = new int[N*N];
 
-        for(int i = 0; i < N; i++) {
-            input[i] = Integer.parseInt(br.readLine());
+        for(int i=0; i<N; i++) {
+            origin[i] = Integer.parseInt(br.readLine());
         }
-        Arrays.sort(input);
+        Arrays.sort(origin);
         
         int idx = 0;
-        for (int i = 0; i < N; i++) {
-            for (int j = i; j < N; j++) {
-                sumArr[idx++] = input[i] + input[j];
+        for (int i=0; i<N; i++) {
+            for (int j=i; j<N; j++) {
+                twoSum[idx++] = origin[i] + origin[j];
             }
         }
-        Arrays.sort(sumArr, 0, idx - 1);
+        Arrays.sort(twoSum, 0, idx - 1);
 
-        for(int i = N -1; 0 <= i; i--){
-            for(int j = i; 0 <= j; j--){
-                if(Arrays.binarySearch(sumArr, 0, idx -1, input[i]-input[j]) < 0) 
+        for(int i=N-1; i>=0; i--){
+            for(int j=i; j>=0; j--){
+                if(Arrays.binarySearch(twoSum, 0, idx -1, origin[i]-origin[j]) < 0) 
                 	continue;
-                System.out.println(input[i]);
+                System.out.println(origin[i]);
                 return;
             }
         }
