e4d38b386444cbfbf313f60aec97936d75daf8b6
fsm12 <74345771+fsm12@users.noreply.github.com>
2024-04-03 12:20:44 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 9875.50 ms, Memory: 126 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
@@ -1,25 +1,12 @@
-/*
-
-enroll ["john", "mary", "edward", "sam", "emily", "jaimie", "tod", "young"]	
-referral ["-", "-", "mary", "edward", "mary", "mary", "jaimie", "edward"]	
-seller ["young", "john", "tod", "emily", "mary"]	
-amount [12, 4, 2, 5, 10]	
-
-result [360, 958, 108, 0, 450, 18, 180, 1080]
-
-위 / 방향성
-*/
-
 import java.util.*;
 
 class Solution {
     static List<Integer>[] adjList;
     static Map<String, Integer> nameIdxMap;
     static int[] ans;
-    
+    static int i=0;
     
     public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
-        
         nameIdxMap = new HashMap<>();
         
         int N = 0;
@@ -32,13 +19,13 @@ class Solution {
             adjList[i] = new ArrayList<>();
         }
         
-        
-        int i=0;
+        i=0;
         for(String ref : referral){
             if(ref.equals("-")){
                 i++;
                 continue;
             }
+            
             int nameIdx = nameIdxMap.get(enroll[i++]);
             int refIdx = nameIdxMap.get(ref);
             adjList[nameIdx].add(refIdx);
@@ -49,20 +36,18 @@ class Solution {
         for(String sell : seller){
             int nameIdx = nameIdxMap.get(sell);
             int money = amount[i++]*100;
-            moneyDistribution(nameIdx, money);
+            moneyDistri(nameIdx, money);
         }
-        
         return ans;
     }
     
-    public static void moneyDistribution(int idx, int money){
+    public static void moneyDistri(int idx, int money){
         int upMoney = money/10;
         ans[idx] += money - upMoney;
         
-        if(adjList[idx].size()==0){
-            //ans[idx] += upMoney;
+        if(adjList[idx].size()==0) 
             return;
-        }
-        moneyDistribution(adjList[idx].get(0), upMoney);
+        
+        moneyDistri(adjList[idx].get(0), upMoney);
     }
 }
