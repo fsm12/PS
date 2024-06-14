d39937cb3a0a7b766ade7fe30dea65359e2f6f65
fsm12 <74345771+fsm12@users.noreply.github.com>
2024-06-15 05:07:08 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 80 ms, Memory: 11572 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -1,22 +1,25 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-public class Main {
-	static int N;
-	
-    public static void main(String[] args) throws IOException {
-    	// System.setIn(new FileInputStream("src/input.txt"));
-    	
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	N = Integer.parseInt(br.readLine());
-    	
-    	StringBuilder sb = new StringBuilder();
-    	for(int i=0; i<N; i++) {
-    		for(int j=0; j<=i; j++) {
-    			sb.append("*");
-    		}
-    		sb.append("\n");
-    	}
-    	System.out.print(sb.toString());
+public class Main{
+    static int N;
+    static StringBuilder ans;
+    
+    public static void main(String[] args) throws Exception{
+        // System.setIn(new FileInputStream("input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        N = Integer.parseInt(st.nextToken());
+        
+        ans = new StringBuilder();
+        for(int i=1; i<=N; i++){
+            for(int j=0; j<i; j++){
+                ans.append("*");
+            }
+            ans.append("\n");
+        }
+
+        System.out.println(ans);
     }
 }
