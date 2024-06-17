384b6e816ef91e74ff542c57ac65c23f1c861594
fsm12 <74345771+fsm12@users.noreply.github.com>
2024-06-18 04:34:23 +0900

    [Bronze V] Title: 콜센터, Time: 68 ms, Memory: 11208 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	static String printStr;
+	
+	public static void main(String[] args) throws Exception{
+		// System.setIn(new FileInputStream("input.txt"));
+		
+		printStr = "     /~\\ \n"
+				+ "    ( oo| \n"
+				+ "    _\\=/_ \n"
+				+ "   /  _  \\ \n"
+				+ "  //|/.\\|\\\\ \n"
+				+ " ||  \\ /  || \n"
+				+ "============ \n"
+				+ "|          | \n"
+				+ "|          | \n"
+				+ "|          |";
+		System.out.println(printStr);
+	}
+}
