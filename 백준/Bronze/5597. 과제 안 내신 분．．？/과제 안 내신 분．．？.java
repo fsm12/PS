8c17d95b407086c00da161d770d661f6f3ad1266
fsm12 <74345771+fsm12@users.noreply.github.com>
2024-07-02 21:22:43 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 64 ms, Memory: 11424 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -1,20 +1,21 @@
+import java.util.*;
 import java.io.*;
 
 public class Main{
-    static int[] arr = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30};
-
-    public static void main(String[] args) throws IOException {
+    
+    public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        for (int i = 0; i < 28; i++) {
-            int in = Integer.parseInt(br.readLine());
-            arr[in-1] = 0;
+        
+        boolean[] status = new boolean[31];
+        for(int i=0; i<28; i++){
+            int a = Integer.parseInt(br.readLine());
+            status[a] = true;
         }
-
-        for (int i = 0; i < arr.length; i++) {
-            if(arr[i]!=0){
-                System.out.println(i+1);
-            }
+        
+        for(int i=1; i<=30; i++){
+            if(!status[i])
+                System.out.println(i);
         }
+        
     }
 }
